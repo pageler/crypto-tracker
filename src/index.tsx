a9c53a4875a884fec67e3e97daad11fd747fbcec
John Paul Pageler Jr <harakido1@yahoo.com>
2023-11-01 05:28:18 -0400

    Setup Context as CryptoContext

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import "react-alice-carousel/lib/alice-carousel.css";
 import { BrowserRouter } from "react-router-dom";
+import { CryptoContext } from "./CryptoContext";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
@@ -11,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <CryptoContext>
+                <App />
+            </CryptoContext>
         </BrowserRouter>
     </React.StrictMode>
 );
