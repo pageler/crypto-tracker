10ec0e320f0be27d2047606b0dea200838ab5a86
John Paul Pageler Jr <harakido1@yahoo.com>
2023-11-01 05:14:00 -0400

    Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,33 @@
 import "./App.css";
+import { makeStyles } from "tss-react/mui";
+import { Route, Routes } from "react-router-dom";
+import { Header } from "./components/Header";
+import { HomePage } from "./pages/HomePage";
+import { CoinPage } from "./pages/CoinPage";
 
 function App() {
+    const useStyles = makeStyles()(() => {
+        return {
+            App: {
+                backgroundColor: "#14161a",
+                color: "white",
+                minHeight: "100vh",
+            },
+        };
+    });
+
+    const { classes } = useStyles();
+
     return (
-        <div className="App">
-            <h1>Crypto Tracker</h1>
-        </div>
+        <>
+            <div className={classes.App}>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/coins/:id" element={<CoinPage />} />
+                </Routes>
+            </div>
+        </>
     );
 }
 
