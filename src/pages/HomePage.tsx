218dce5ede8f283d1bf06a227771fcbc40e9e791
John Paul Pageler Jr <harakido1@yahoo.com>
2023-11-11 03:29:37 -0500

    Setup lazy-loading

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,13 +1,22 @@
-import { CoinTable } from "../components/CoinTable";
+import { Suspense, lazy } from "react";
 import { Banner } from "../components/banner/Banner";
 
 type HomePageProps = {};
 
+const CoinTable = lazy(() =>
+    import("../components/CoinTable").then(({ CoinTable }) => ({
+        default: CoinTable,
+    }))
+);
+
 export const HomePage = (props: HomePageProps) => {
     return (
-        <>
+        <div>
             <Banner />
-            <CoinTable />
-        </>
+
+            <Suspense fallback={<div>Loading...</div>}>
+                <CoinTable />
+            </Suspense>
+        </div>
     );
 };
